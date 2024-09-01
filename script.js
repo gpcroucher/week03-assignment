0ebed582f600691a37b0e674ab33ca31423cf395
George Croucher <gpcroucher@gmail.com>
2024-09-01 13:01:19 +0100

    comments, upgrade counts, labels

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cookieTotalElement = document.getElementById("cookieTotal");
+const CPSElement = document.getElementById("CPS");
 const cookieButton = document.getElementById("cookieButton");
 const upgradeMenu = document.getElementById("upgradeMenu");
 
@@ -6,37 +7,44 @@ let cookieTotal = 0;
 let cookiesPerClick = 1;
 let cookiesPerSecond = 0;
 let upgradesBought = [];
-load();
 
 cookieButton.addEventListener("click", function () {
   addCookies(cookiesPerClick);
 });
 
+// load the game state, start update and save timers and build the menu
+load();
 setInterval(update, 1000);
 setInterval(save, 15000);
+getUpgrades(upgradesBought);
 
-getUpgrades();
-
+// add cookies to total - depends on cookies per click
 function addCookies(amount) {
   cookieTotal += amount;
   cookieTotalElement.innerText = cookieTotal;
 }
 
+// buy an upgrade if we can afford it
 function buyUpgrade(upgrade) {
   if (cookieTotal < upgrade.cost) {
     console.log("Not enough cookies to purchase:", upgrade.name);
-  } else {
-    if (!upgradesBought[upgrade.id]) {
-      upgradesBought[upgrade.id] = upgrade;
-      upgradesBought[upgrade.id].count = 0;
-    }
-    upgradesBought[upgrade.id].count++;
-    cookiesPerSecond = calculateCPS();
-    cookieTotal -= upgrade.cost;
-    console.log("Bought upgrade:", upgrade.name);
+    return;
   }
+  if (!upgradesBought[upgrade.id]) {
+    upgradesBought[upgrade.id] = upgrade;
+    upgradesBought[upgrade.id].count = 0;
+  }
+  upgradesBought[upgrade.id].count++;
+  cookiesPerSecond = calculateCPS();
+  cookieTotal -= upgrade.cost;
+
+  let ownedLabel = document.getElementById(`ownedLabel${upgrade.id}`);
+  ownedLabel.innerText = Number(ownedLabel.innerText) + 1;
+
+  console.log("Bought upgrade:", upgrade.name);
 }
 
+// calculate cookies per second from list of bought upgrades
 function calculateCPS() {
   let CPS = 0;
   for (upgrade of upgradesBought) {
@@ -47,7 +55,8 @@ function calculateCPS() {
   return CPS;
 }
 
-async function getUpgrades() {
+// fetch upgrades from API and build the upgrade menu
+async function getUpgrades(upgradesBoughtParam) {
   // get upgrades from API
   const response = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
@@ -56,7 +65,7 @@ async function getUpgrades() {
 
   // build upgrade menu
   for await (const upgrade of upgradeList) {
-    let upgradeBox = document.createElement("div");
+    let upgradeBox = document.createElement("button");
     upgradeBox.classList.add("upgrade-box");
     upgradeBox.id = `upgradeBox${upgrade.id}`;
     upgradeBox.addEventListener("click", function () {
@@ -67,19 +76,29 @@ async function getUpgrades() {
     nameLabel.innerText = upgrade.name;
 
     let costLabel = document.createElement("p");
-    costLabel.innerText = upgrade.cost;
+    costLabel.innerText = `Cost: ${upgrade.cost}`;
 
     let CPSLabel = document.createElement("p");
-    CPSLabel.innerText = upgrade.increase;
+    CPSLabel.innerText = `+${upgrade.increase} per second`;
+
+    let ownedLabel = document.createElement("p");
+    ownedLabel.id = `ownedLabel${upgrade.id}`;
+    if (upgradesBoughtParam[upgrade.id]) {
+      ownedLabel.innerText = upgradesBoughtParam[upgrade.id].count;
+    } else {
+      ownedLabel.innerText = 0;
+    }
 
     upgradeBox.appendChild(nameLabel);
     upgradeBox.appendChild(costLabel);
     upgradeBox.appendChild(CPSLabel);
+    upgradeBox.appendChild(ownedLabel);
     upgradeMenu.appendChild(upgradeBox);
   }
   return upgradeList;
 }
 
+// restore game state from local storage
 function load() {
   if (localStorage.getItem("cookieTotal")) {
     cookieTotal = Number(localStorage.getItem("cookieTotal"));
@@ -95,6 +114,8 @@ function load() {
   }
 }
 
+// store game state in local storage
+// happens every 15 seconds
 function save() {
   cookiesPerSecond = calculateCPS();
   localStorage.setItem("cookieTotal", cookieTotal);
@@ -103,7 +124,10 @@ function save() {
   localStorage.setItem("upgradesBought", JSON.stringify(upgradesBought));
 }
 
+// update the total cookies and cookies per second readouts
+// happens every second
 function update() {
   cookieTotal += cookiesPerSecond;
   cookieTotalElement.innerText = cookieTotal;
+  CPSElement.innerText = cookiesPerSecond;
 }
